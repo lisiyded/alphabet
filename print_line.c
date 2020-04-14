e48f90d8942bcb01bcb0edc7461414eeb0e77cb2
Kris <n-me.ru>
2020-04-15 02:16:39 +0300

    line1notjob

---
+++ print_line.c
@@ -0,0 +1,50 @@
+#include <unistd.h>
+
+int	ft_strlen(char *s)
+
+{
+	int len;
+
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
+
+void	ft_putchar(int c)
+
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *s)
+
+{	
+	int len;
+
+
+	len = ft_strlen(s);
+	write(1, &s[len], len);
+}
+
+void	ft_putendl(char *s)
+
+{
+	ft_putstr(s);
+	write(1, "\n", 1);
+}
+
+int	main(void)
+
+{
+	ft_putendl("This string has a nawline sumbol at the end (;");
+	ft_putstr("And this line ands here!");
+	return(0);
+}
+
+
+
---
+++ print_line2.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+
+//int	ft_strlen(char *s)
+//
+//{
+//	int len;
+//
+//
+//	len = 0;
+//	while (s[len] != '\0')
+//	{
+//		len++;
+//	}
+//
+//	return (len);
+//}
+
+void	ft_putchar(int c)
+
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *s)
+
+{
+	int i;
+
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		write(1, &s[i], 1);
+		i++;
+	}
+}
+
+void	ft_putendl(char *s)
+
+{
+	ft_putstr(s);
+	write(1, "\n", 1);
+}
+
+int	main(void)
+
+{
+	ft_putendl("This string has a nawline sumbol at the end (;");
+	ft_putstr("And this line ands here!");
+	return(0);
+}
